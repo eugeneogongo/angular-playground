802f696780fb1ee991fd83e4af9697d53f3fec19
Neptune <eugeneogongo@live.com>
2020-04-19 22:20:07 +0300

    Changed padding

--- src/app/body/hero/hero.component.ts
+++ src/app/body/hero/hero.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {GetLatestNewsService} from '../../services/get-latest-news.service';
 import {INew} from '../../services/INew';
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-hero',
@@ -13,7 +14,12 @@ export class HeroComponent implements OnInit {
   selectedArticle: INew;
   url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4b696c810c9d4abb9b449986f6459402';
 
-  constructor(private newsService: GetLatestNewsService) {
+  constructor(private newsService: GetLatestNewsService, private meta: Meta) {
+    this.meta.addTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.meta.addTag({ name: 'twitter:site', content: '@eugene_odera' });
+    this.meta.addTag({ name: 'twitter:title', content: 'News API' });
+    this.meta.addTag({ name: 'twitter:description', content: 'Made by Eugene With Love' });
+    this.meta.addTag({ name: 'twitter:image', content: 'https://alligator.io/images/front-end-cover.png' });
   }
 
   ngOnInit(): void {
